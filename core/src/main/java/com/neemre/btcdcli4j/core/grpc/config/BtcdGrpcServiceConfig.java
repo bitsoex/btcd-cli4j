98b727ea445aa74b73195d9658ed80049f904a1a
vinicius-almeida-bitso <vinicius.almeida@bitso.com>
2022-09-23 19:23:43 -0300

    remove no args constructor

--- core/src/main/java/com/neemre/btcdcli4j/core/grpc/config/BtcdGrpcServiceConfig.java
+++ core/src/main/java/com/neemre/btcdcli4j/core/grpc/config/BtcdGrpcServiceConfig.java
@@ -3,13 +3,10 @@ package com.neemre.btcdcli4j.core.grpc.config;
 import com.bitso.model.BtcdServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 
 /**
  * Configuration to create a gRPC client for the {@code <currency-node>.bitsocluster<env>.bitsoops.com} service
  */
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BtcdGrpcServiceConfig {
 
     /**
